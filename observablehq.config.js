100faa8524506d86c56df32d74560db266a8e115
E M Greeff <emgreeff@datadash.cloud>
2026-02-06 15:45:04 -0500

    On branch dev Your branch is up to date with 'origin/dev'.
    Changes to be committed:
    	renamed:    .github/workflows/static.yml -> .github/workflows/deploy.yml
    	modified:   observablehq.config.js
    	modified:   package-lock.json
    	modified:   package.json
    	new file:   src/data/location.json.js
    	new file:   src/example-location.md

--- observablehq.config.js
+++ observablehq.config.js
@@ -1,20 +1,18 @@
 // See https://observablehq.com/framework/config for documentation.
 export default {
-  // The app’s title; used in the sidebar and webpage titles.
   title: "Modster",
-
   // The pages and sections in the sidebar. If you don’t specify this option,
   // all pages will be listed in alphabetical order. Listing pages explicitly
   // lets you organize them into sections and have unlisted pages.
-  // pages: [
-  //   {
-  //     name: "Examples",
-  //     pages: [
-  //       {name: "Dashboard", path: "/example-dashboard"},
-  //       {name: "Report", path: "/example-report"}
-  //     ]
-  //   }
-  // ],
+  pages: [
+    {
+      name: "Examples",
+      pages: [
+        {name: "Location", path: "/example-location"},
+        // {name: "Report", path: "/example-report"}
+      ]
+    }
+  ],
 
   // Content to add to the head of the page, e.g. for a favicon:
   head: '<link rel="icon" href="observable.png" type="image/png" sizes="32x32">',
@@ -28,8 +26,8 @@ export default {
   // footer: "Built with Observable.", // what to show in the footer (HTML)
   // sidebar: true, // whether to show the sidebar
   // toc: true, // whether to show the table of contents
-  // pager: true, // whether to show previous & next links in the footer
-  // output: "dist", // path to the output root for build
+  pager: false, // whether to show previous & next links in the footer
+  output: "dist", // path to the output root for build
   // search: true, // activate search
   // linkify: true, // convert URLs in Markdown to links
   // typographer: false, // smart quotes and other typographic improvements
