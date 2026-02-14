7796eddaced3ef88cd1655095d5746da563c2651
E M Greeff <emgreeff@datadash.cloud>
2026-02-14 13:55:22 -0500

    On branch main Your branch is behind 'origin/main' by 1 commit, and can be fast-forwarded.   (use "git pull" to update your local branch)
    Changes to be committed:
    	deleted:    .github/workflows/deploy.yml
    	modified:   .gitignore
    	modified:   observablehq.config.js

--- observablehq.config.js
+++ observablehq.config.js
@@ -24,7 +24,7 @@ export default {
   sidebar: true, // whether to show the sidebar
   toc: true, // whether to show the table of contents
   // pager: false, // whether to show previous & next links in the footer
-  output: "dist", // path to the output root for build
+  output: "docs", // path to the output root for build
   search: true, // activate search
   linkify: true, // convert URLs in Markdown to links
   typographer: true, // smart quotes and other typographic improvements
